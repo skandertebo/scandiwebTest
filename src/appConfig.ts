2b2b747b82cc706599a58339d7654328140b4b95
skandertebo <alexandertebourb@gmail.com>
2023-03-16 21:34:47 +0100

    fixing ssl problems

--- src/appConfig.ts
+++ src/appConfig.ts
@@ -1,4 +1,4 @@
 export const MODE : 'DEV' | 'PROD' = 'PROD'; // DEV or PROD
 
 
-export const apiRoute = MODE === 'PROD' ? 'http://skandertebotest.c1.biz' : 'http://localhost:80/backendTestScandiweb';
+export const apiRoute = MODE === 'PROD' ? 'http://skandertebotest.atwebpages.com/' : 'http://localhost:80/backendTestScandiweb';
