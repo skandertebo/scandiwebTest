8a877eb03503914d30f20daadec5f8f132e9b307
skandertebo <alexandertebourb@gmail.com>
2023-03-23 00:10:14 +0100

    removed button disability

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,23 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
   ],
-  'overrides': [
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'plugins': [
-    'react',
-    '@typescript-eslint'
-  ],
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ]
-  }
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
 };
--- src/Components/HomeHeader.tsx
+++ src/Components/HomeHeader.tsx
@@ -1,39 +1,43 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { usePendingToDelete } from '../Context/PendingToDeleteContext';
-import {useCallback} from 'react';
+import { useCallback } from 'react';
 import massDeleteProducts from '../ApiCalls/massDeleteProducts';
 import { useProducts } from '../Context/ProductsContext';
 import { ProductsContext } from '../Types/ProductsContextType';
 
 const HomeHeader: React.FC = () => {
-  
   const navigate = useNavigate();
-  const {pendingToDelete, clearPendingToDelete} = usePendingToDelete();
-  const {products, setProducts} = useProducts() as ProductsContext;  
+  const { pendingToDelete, clearPendingToDelete } = usePendingToDelete();
+  const { products, setProducts } = useProducts() as ProductsContext;
 
-  const handleDelete = useCallback(
-    () => {
-      // generate a string with skus to delete separated by comma
-      massDeleteProducts(pendingToDelete).then((res)=>{
-        if(res.message==='Success'){
+  const handleDelete = useCallback(() => {
+    // generate a string with skus to delete separated by comma
+    pendingToDelete.length > 0 &&
+      massDeleteProducts(pendingToDelete).then((res) => {
+        if (res.message === 'Success') {
           // delete the products from the products array
-          products && setProducts(products.filter((p)=>!pendingToDelete.includes(p)));
+          products &&
+            setProducts(products.filter((p) => !pendingToDelete.includes(p)));
           // clear the pendingToDelete array
           clearPendingToDelete();
         }
       });
-    },
-    [pendingToDelete],
-  );
-  
+  }, [pendingToDelete]);
 
-  return(
+  return (
     <div className="header">
       <h1>Product Add</h1>
-      <div className='action-buttons'>
-        <button className="btn btn-primary" onClick={()=>navigate('/addproduct')}>ADD</button>
-        <button className="btn btn-danger" onClick={handleDelete} disabled={pendingToDelete.length===0}>MASS DELETE</button>
+      <div className="action-buttons">
+        <button
+          className="btn btn-primary"
+          onClick={() => navigate('/addproduct')}
+        >
+          ADD
+        </button>
+        <button className="btn btn-danger" onClick={handleDelete}>
+          MASS DELETE
+        </button>
       </div>
     </div>
   );
